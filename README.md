ffa0769da301fcaf7971470ed4789db74c6dbfac
Thiago Conte <tcr2@cin.ufpe.br>
2022-11-11 16:11:35 -0500

    Criacao unica de contas por cpf

---
+++ README.md
@@ -0,0 +1,12 @@
+# API Financeira
+
+# Requisitos
+
+ - [] Criar conta
+ - [] Buscar extrato bancario
+ - [] Realizar um deposito
+ - [] Realizar um saque
+ - [] Buscar extrato por data
+ - [] Atualizar dados da conta
+ - [] Obter dados da conta
+ - [] Deletar uma conta
--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
 const express = require("express");
 
+// V4 gera numeros aleatorios (otimo para um id)
+const { v4: uuidv4 } = require("uuid");
+
 const app = express();
 
-app.get("/", (request, response) => {
-    return response.json({ message: "Teste" });
+app.use(express.json());
+
+//"banco de dados" temporario
+const custumers = [];
+
+app.post("/account", (request, response) => {
+    const { cpf, name } = request.body;
+
+    //some() faz uma busca e verifica o que foi especificado dentro dos parenteses
+    //funcao utilizada para verificar se uma conta ja existe com o cpf fornecido pelo usuario
+    const custumerAlreadyExists = custumers.some(
+        (custumer) => custumer.cpf === cpf
+    );
+
+    if(custumerAlreadyExists) {
+        return response.status(400).json({ error: "Customer already exists" });
+    }
+
+    custumers.push({
+        cpf,
+        name,
+        id: uuidv4(),
+        statement: []
+    });
+    return response.status(201).send();
 });
 
 app.listen(3333);
