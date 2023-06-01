66af86c125a65d2636ec611c39ee9f81ddbb07be
Thiago Conte <tcr2@cin.ufpe.br>
2023-06-01 14:25:35 -0300

    Atualizar e obter dados da conta adicionados

--- README.md
+++ README.md
@@ -7,6 +7,6 @@
  - [X] Realizar um deposito
  - [X] Realizar um saque
  - [X] Buscar extrato por data
- - [] Atualizar dados da conta
- - [] Obter dados da conta
+ - [X] Atualizar dados da conta
+ - [X] Obter dados da conta
  - [] Deletar uma conta
--- src/index.js
+++ src/index.js
@@ -123,5 +123,19 @@ app.get("/statement/date", (request, response) => {
 
 });
 
+app.put("/account", (request, response) => {
+    const { name } = request.body;
+    const { customer } = request;
+
+    customer.name = name;
+
+    return response.status(201).send();
+});
+
+app.get("/account", (request, response) => {
+    const { customer } = request;
+
+    return response.json(customer);
+});
 
 app.listen(3333);
